76d7275e1398669189eb9e25b4f776ca0ee42f68
Emmanuel Olagoke <emma.goke05@gmail.com>
2022-10-19 05:10:38 +0100

    Added a function that check the argument list

---
+++ checker.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * check - Receives the main string and all the necessary parameters to
+ * print a formated string.
+ * @format: A string containing all the desired characters.
+ * @type: A list of all the possible functions.
+ * @arg: A list containing all the arguments passed to the program.
+ * Return: A total count of the characters printed.
+ */
+int check(const char *format, format_t type[], va_list arg)
+{
+	int i, j, len = 0;
+
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			for (j = 0; type[j].id != NULL; j++)
+			{
+				if (format[i + 1] == type[j].id[0])
+				{
+					type[j].print(arg);
+					len += 1;
+				}
+			}
+			i++;
+		}
+		else
+		{
+			_putchar(format[i]);
+			len += 1;
+		}
+	}
+	return (len);
+}
