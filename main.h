6e343b13cca60577ce85cd76685e1e33b43ffb81
Emmanuel Olagoke <emma.goke05@gmail.com>
2022-10-19 02:36:58 +0100

    This contains function prototype

---
+++ main.h
@@ -0,0 +1,25 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdarg.h>
+
+struct format
+{
+	char *id;
+	void (*print)(va_list);
+};
+typedef struct format format_t;
+int _putchar(char c);
+
+void print_s(va_list);
+void print_c(va_list);
+void print_string(char *);
+int _printf(const char *format, ...);
+void print_int(int);
+void print_d_i(va_list);
+void _binary(unsigned int);
+void print_b(va_list);
+void print_per(va_list);
+
+#endif
